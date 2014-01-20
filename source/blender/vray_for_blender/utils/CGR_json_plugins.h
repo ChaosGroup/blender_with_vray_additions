d66a62bb05dc81d29eac9e0c566bdea4c838a6e0
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-01-20 18:56:17 +0200

    add: json plugin description parser

---
+++ source/blender/vray_for_blender/utils/CGR_json_plugins.h
@@ -0,0 +1,45 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2010 Blender Foundation.
+ * All rights reserved.
+ *
+ * Contributor(s): Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+#ifndef CGR_JSON_PLUGINS_H
+#define CGR_JSON_PLUGINS_H
+
+#ifdef _MSC_VER
+#include <boost/config/compiler/visualc.hpp>
+#endif
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+
+#include <cassert>
+#include <exception>
+#include <iostream>
+#include <sstream>
+
+
+void  PrintTree(boost::property_tree::ptree &pt);
+int   ReadPluginDesc(const char *pluginIDName, boost::property_tree::ptree &pTree);
+
+#endif // CGR_JSON_PLUGINS_H
