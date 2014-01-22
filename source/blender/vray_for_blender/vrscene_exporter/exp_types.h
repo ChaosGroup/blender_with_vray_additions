ad22e4a4f68ef7e36fa6d2c97258bf910984b47b
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-01-22 14:40:12 +0200

    add: VRayExportable base class

---
+++ source/blender/vray_for_blender/vrscene_exporter/exp_types.h
@@ -0,0 +1,48 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2010 Blender Foundation.
+ * All rights reserved.
+ *
+ * Contributor(s): Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+#ifndef CGR_EXP_TYPES_H
+#define CGR_EXP_TYPES_H
+
+#include "murmur3.h"
+#include <string>
+
+
+class VRayExportable {
+public:
+	virtual      ~VRayExportable() {}
+
+	MHash         getHash() const { return hash; }
+	const char   *getName() const { return name.c_str(); }
+
+	virtual void  buildHash()=0;
+
+protected:
+	std::string   name;
+	MHash         hash;
+
+};
+
+#endif // CGR_EXP_TYPES_H
