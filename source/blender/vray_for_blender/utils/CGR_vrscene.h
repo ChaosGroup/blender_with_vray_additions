e4cd34154cea41a7f61949e35ab4e5044efa89a4
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-01-23 19:55:49 +0200

    fix: removed useless extern

--- source/blender/vray_for_blender/utils/CGR_vrscene.h
+++ source/blender/vray_for_blender/utils/CGR_vrscene.h
@@ -5,19 +5,11 @@
 
 #define TRANSFORM_HEX_SIZE  129
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void  GetDoubleHex(float f, char *buf);
 void  GetFloatHex(float f, char *buf);
 void  GetVectorHex(float f[3], char *buf);
 void  GetTransformHex(float m[4][4], char *buf);
 
 char* GetStringZip(const u_int8_t *buf, unsigned bufLen);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
 
 #endif // CGR_VRSCENE_H
