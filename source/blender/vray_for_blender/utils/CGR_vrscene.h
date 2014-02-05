0575c36ad98e236ba80412d81228559b6b9d6f4a
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-02-05 15:48:48 +0200

    add: GetHex func

--- source/blender/vray_for_blender/utils/CGR_vrscene.h
+++ source/blender/vray_for_blender/utils/CGR_vrscene.h
@@ -21,6 +21,7 @@ void  GetFloatHex(float f, char *buf);
 void  GetVectorHex(float f[3], char *buf);
 void  GetTransformHex(float m[4][4], char *buf);
 
+char* GetHex(const u_int8_t *buf, unsigned bufLen);
 char* GetStringZip(const u_int8_t *buf, unsigned bufLen);
 char* GetFloatArrayZip(float *data, size_t size);
 
