3a930ed7ee8b914b9e337943c8644bd6ec3ab3be
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-05-14 22:29:22 +0300

    fix: comment removed; small fix

--- source/blender/vray_for_blender/vrscene_exporter/nodes/specials/TexLayered.cpp
+++ source/blender/vray_for_blender/vrscene_exporter/nodes/specials/TexLayered.cpp
@@ -25,46 +25,6 @@
 
 std::string VRayNodeExporter::exportVRayNodeTexLayered(BL::NodeTree ntree, BL::Node node)
 {
-//	pluginName = clean_string("nt%sn%s" % (nodetree.name, node.name))
-
-//	textures    = []
-//	blend_modes = []
-
-//	for inputSocket in node.inputs:
-//		if not inputSocket.is_linked:
-//			continue
-//		tex = WriteConnectedNode(bus, nodetree, inputSocket)
-
-//		# XXX: For some reason TexLayered doesn't like ::out_smth
-//		semiPos = tex.find("::")
-//		if semiPos != -1:
-//			tex = tex[:semiPos]
-
-//		textures.append(tex)
-//		blend_modes.append(inputSocket.value)
-
-//	alpha        = WriteConnectedNode(bus, nodetree, node.inputs['Alpha'])
-//	alpha_mult   = WriteConnectedNode(bus, nodetree, node.inputs['Alpha Mult'])
-//	alpha_offset = WriteConnectedNode(bus, nodetree, node.inputs['Alpha Offset'])
-//	nouvw_color  = WriteConnectedNode(bus, nodetree, node.inputs['No UV Color'])
-//	color_mult   = WriteConnectedNode(bus, nodetree, node.inputs['Color Mult'])
-//	color_offset = WriteConnectedNode(bus, nodetree, node.inputs['Color Offset'])
-
-//	o.set('TEXTURE', 'TexLayered', pluginName)
-//	o.writeHeader()
-//	o.writeAttibute('textures', "List(%s)" % ','.join(reversed(textures)))
-//	o.writeAttibute('blend_modes', "ListInt(%s)" % ','.join(reversed(blend_modes)))
-//	o.writeAttibute('alpha_from_intensity', node.alpha_from_intensity)
-//	o.writeAttibute('invert', node.invert)
-//	o.writeAttibute('invert_alpha', node.invert_alpha)
-//	o.writeAttibute('alpha', alpha)
-//	o.writeAttibute('alpha_mult', alpha_mult)
-//	o.writeAttibute('alpha_offset', alpha_offset)
-//	o.writeAttibute('nouvw_color', nouvw_color)
-//	o.writeAttibute('color_mult', color_mult)
-//	o.writeAttibute('color_offset', color_offset)
-//	o.writeFooter()
-
 	std::string pluginName = StripString("NT" + ntree.name() + "N" + node.name());
 
 	StrVector textures;
@@ -75,7 +35,7 @@ std::string VRayNodeExporter::exportVRayNodeTexLayered(BL::NodeTree ntree, BL::N
 
 		BL::NodeSocket texSock = VRayNodeExporter::getSocketByName(node, texSockName);
 		if(NOT(texSock))
-			break;
+			continue;
 
 		if(NOT(texSock.is_linked()))
 			continue;
