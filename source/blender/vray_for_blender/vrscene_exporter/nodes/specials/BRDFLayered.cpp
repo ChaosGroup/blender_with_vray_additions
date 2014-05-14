d34ad65f23ca4ca8335f7518bd17f2fdf887697a
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-05-14 22:53:20 +0300

    fix: BRDFLayered support

--- source/blender/vray_for_blender/vrscene_exporter/nodes/specials/BRDFLayered.cpp
+++ source/blender/vray_for_blender/vrscene_exporter/nodes/specials/BRDFLayered.cpp
@@ -25,5 +25,69 @@
 
 std::string VRayNodeExporter::exportVRayNodeBRDFLayered(BL::NodeTree ntree, BL::Node node)
 {
-	return "NULL";
+	std::string pluginName = StripString("NT" + ntree.name() + "N" + node.name());
+
+	StrVector brdfs;
+	StrVector weights;
+
+	for(int i = 1; i <= CGR_MAX_LAYERED_BRDFS; ++i) {
+		std::string brdfSockName   = boost::str(boost::format("BRDF %i")   % i);
+		std::string weigthSockName = boost::str(boost::format("Weight %i") % i);
+
+		BL::NodeSocket brdfSock   = VRayNodeExporter::getSocketByName(node, brdfSockName);
+		if(NOT(brdfSock))
+			continue;
+		if(NOT(brdfSock.is_linked()))
+			continue;
+
+		std::string brdf   = VRayNodeExporter::exportLinkedSocket(ntree, brdfSock);
+		std::string weight = "1.0";
+
+		BL::NodeSocket weightSock = VRayNodeExporter::getSocketByName(node, weigthSockName);
+		if(weightSock.is_linked()) {
+			weight = VRayNodeExporter::exportLinkedSocket(ntree, weightSock);
+		}
+		else {
+			weight = boost::str(boost::format("%sW%i") % pluginName % i);
+
+			float       weigthValue = RNA_float_get(&weightSock.ptr, "value");
+			std::string weightColor = boost::str(boost::format("AColor(%.6f,%.6f,%.6f,1.0)")
+												 % weigthValue % weigthValue % weigthValue);
+
+			std::stringstream weigthPlugin;
+			weigthPlugin << "\n"   << "TexAColor" << " " << weight << " {";
+			weigthPlugin << "\n\t" << "texture" << "=" << weightColor << ";";
+			weigthPlugin << "\n}\n";
+
+			PYTHON_PRINT(VRayNodeExporter::m_exportSettings->m_fileMat, weigthPlugin.str().c_str());
+		}
+
+		brdfs.push_back(brdf);
+		weights.push_back(weight);
+	}
+
+	AttributeValueMap manualAttrs;
+	manualAttrs["brdfs"]   = boost::str(boost::format("List(%s)") % boost::algorithm::join(brdfs, ","));
+	manualAttrs["weights"] = boost::str(boost::format("List(%s)") % boost::algorithm::join(weights, ","));
+
+	manualAttrs["transparency"]  = VRayNodeExporter::exportSocket(ntree, VRayNodeExporter::getSocketByName(node, "Transparency"));
+	manualAttrs["additive_mode"] = boost::str(boost::format("%i") % RNA_boolean_get(&node.ptr, "additive_mode"));
+
+	std::stringstream plugin;
+
+	plugin << "\n" << "BRDFLayered" << " " << pluginName << " {";
+
+	AttributeValueMap::const_iterator attrIt;
+	for(attrIt = manualAttrs.begin(); attrIt != manualAttrs.end(); ++attrIt) {
+		const std::string attrName  = attrIt->first;
+		const std::string attrValue = attrIt->second;
+
+		plugin << "\n\t" << attrName << "=" << VRayExportable::m_interpStart << attrValue << VRayExportable::m_interpEnd << ";";
+	}
+
+	plugin << "\n}\n";
+
+	PYTHON_PRINT(VRayNodeExporter::m_exportSettings->m_fileMat, plugin.str().c_str());
+
+	return pluginName;
 }
