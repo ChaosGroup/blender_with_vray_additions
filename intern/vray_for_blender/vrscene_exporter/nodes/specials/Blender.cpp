91525e3146fc9c27e00f05012e552bbdc123bcac
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-09-10 15:53:17 +0300

    fix: proper curve points

--- intern/vray_for_blender/vrscene_exporter/nodes/specials/Blender.cpp
+++ intern/vray_for_blender/vrscene_exporter/nodes/specials/Blender.cpp
@@ -25,6 +25,13 @@
 // Remap values from [-1.0 .. 1.0] to [0.0 .. 1.0]
 #define REMAP_CURVE_POINT(x) (0.5f * ((x) + 1.0f))
 
+#define MAX_NUM_POINTS 64
+
+struct MyPoint {
+	float x;
+	float y;
+};
+
 
 void VRayNodeExporter::getNodeVectorCurveData(BL::Node node, StrVector &points, StrVector &types)
 {
@@ -34,53 +41,66 @@ void VRayNodeExporter::getNodeVectorCurveData(BL::Node node, StrVector &points, 
 	curveMapping.initialize();
 
 	BL::CurveMap curve = curveMapping.curves[0];
-	if (NOT(curve.points.length()))
+
+	const int numPoints = curve.points.length();
+
+	if (NOT(numPoints))
 		return;
 
+	MyPoint point[MAX_NUM_POINTS];
+
 	BL::CurveMap::points_iterator pointIt;
-	for (curve.points.begin(pointIt); pointIt != curve.points.end(); ++pointIt) {
-		BL::CurveMapPoint point = *pointIt;
+	int p = 0;
+	for (curve.points.begin(pointIt); pointIt != curve.points.end(); ++pointIt, ++p) {
+		BL::CurveMapPoint mapPoint = *pointIt;
 
-		BL::Array<float, 2>                  pointLoc  = point.location();
-		BL::CurveMapPoint::handle_type_enum  pointType = point.handle_type();
+		BL::Array<float, 2> pointLoc = mapPoint.location();
 
-		int pointVRayType = 0;
-		if (pointType == BL::CurveMapPoint::handle_type_AUTO)
-			pointVRayType = 1;
+		point[p].x = REMAP_CURVE_POINT(pointLoc[0]);
+		point[p].y = REMAP_CURVE_POINT(pointLoc[1]);
 
-		const float tanDelta = 0.001;
-
-		const float curvePointLocX = pointLoc[0];
-		const float curvePointLocY = pointLoc[1];
-
-		const float curveTan1LocX = curvePointLocX - tanDelta;
-		const float curveTan1LocY = curve.evaluate(curveTan1LocX);
-
-		const float curveTan2LocX = curvePointLocX + tanDelta;
-		const float curveTan2LocY = curve.evaluate(curveTan2LocX);
-
-		const float pointLocX = REMAP_CURVE_POINT(curvePointLocX);
-		const float pointLocY = REMAP_CURVE_POINT(curvePointLocY);
-
-		// Left handle
-		const float tan1LocX = REMAP_CURVE_POINT(curveTan1LocX);
-		const float tan1LocY = REMAP_CURVE_POINT(curveTan1LocY);
-
-		// Right handle
-		const float tan2LocX = REMAP_CURVE_POINT(curveTan2LocX);
-		const float tan2LocY = REMAP_CURVE_POINT(curveTan2LocY);
-
-		// The control points;
-		// 6 floats for each point: 2D coords of the point itself,
-		// and 2D coords of the left and right tangent.
-		points.push_back(BOOST_FORMAT_FLOAT(pointLocX));
-		points.push_back(BOOST_FORMAT_FLOAT(pointLocY));
-		points.push_back(BOOST_FORMAT_FLOAT(tan1LocX));
-		points.push_back(BOOST_FORMAT_FLOAT(tan1LocY));
-		points.push_back(BOOST_FORMAT_FLOAT(tan2LocX));
-		points.push_back(BOOST_FORMAT_FLOAT(tan2LocY));
+		// The type of the control points:
+		//   0 - free, 1 - bezier, 2 - bezier smooth
+		//
+		const int pointVRayType = (mapPoint.handle_type() == BL::CurveMapPoint::handle_type_AUTO) ? 1 : 0;
 
 		types.push_back(BOOST_FORMAT_INT(pointVRayType));
+	}
+
+	float  deltaX[MAX_NUM_POINTS + 1];
+	float  ySecon[MAX_NUM_POINTS];
+	float  yPrim[MAX_NUM_POINTS];
+	float  d[MAX_NUM_POINTS];
+	float  w[MAX_NUM_POINTS];
+	int    i;
+
+	for(i = 1; i < numPoints; i++)
+		deltaX[i] = point[i].x - point[i-1].x;
+	deltaX[0] = deltaX[1];
+	deltaX[numPoints] = deltaX[numPoints-1];
+	for(i = 1; i < numPoints-1; i++) {
+		d[i] = 2 * (point[i + 1].x - point[i - 1].x);
+		w[i] = 6 * ((point[i + 1].y - point[i].y) / deltaX[i+1] - (point[i].y - point[i - 1].y) / deltaX[i]);
+	}
+	for(i = 1; i < numPoints-2; i++) {
+		w[i + 1] -= w[i] * deltaX[i+1] / d[i];
+		d[i + 1] -= deltaX[i+1] * deltaX[i+1] / d[i];
+	}
+	ySecon[0] = 0;
+	ySecon[numPoints-1] = 0;
+	for(i = numPoints - 2; i >= 1; i--)
+		ySecon[i] = (w[i] - deltaX[i+1] * ySecon[i + 1]) / d[i];
+	for(i = 0; i < numPoints-1; i++)
+		yPrim[i] = (point[i+1].y - point[i].y) / deltaX[i+1] - (deltaX[i+1] / 6.0f) * (2 * ySecon[i] + ySecon[i+1]);
+	yPrim[i] = (point[i].y - point[i-1].y) / deltaX[i] + (deltaX[i] / 6.0f) * ySecon[i-1];
+
+	for(p = 0; p < numPoints; ++p) {
+		points.push_back(BOOST_FORMAT_FLOAT(point[p].x));
+		points.push_back(BOOST_FORMAT_FLOAT(point[p].y));
+		points.push_back(BOOST_FORMAT_FLOAT((-deltaX[p] / 3)));
+		points.push_back(BOOST_FORMAT_FLOAT((-deltaX[p] * yPrim[p] / 3)));
+		points.push_back(BOOST_FORMAT_FLOAT((deltaX[p+1] / 3)));
+		points.push_back(BOOST_FORMAT_FLOAT((deltaX[p+1] * yPrim[p] / 3)));
 	}
 }
 
