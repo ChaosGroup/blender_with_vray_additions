2b8720ea21c99624d09842d311df4a7d9f7b7adb
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2015-01-06 16:05:04 +0200

    fix: check for ptr existance

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -3010,7 +3010,10 @@ void BKE_object_handle_update_ex(EvaluationContext *eval_ctx,
 	int        data_updated = CGR_NONE;
 
 	RNA_pointer_create((ID*)ob, &RNA_Object, ob, &ptr);
-	if (RNA_struct_find_property(&ptr, "vray")) {
+	if (!RNA_struct_find_property(&ptr, "vray")) {
+		ptr = PointerRNA_NULL;
+	}
+	else {
 		ptr = RNA_pointer_get(&ptr, "vray");
 		data_updated = RNA_int_get(&ptr, "data_updated");
 	}
