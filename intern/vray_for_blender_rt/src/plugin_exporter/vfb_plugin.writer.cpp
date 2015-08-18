9c54f627d85ad939f81fc969400b2dc27277c9fd
lyubomir.koev <lyubomir.koev@chaosgroup.com>
2015-08-18 17:56:34 +0300

    fix: style changes

--- intern/vray_for_blender_rt/src/plugin_exporter/vfb_plugin.writer.cpp
+++ intern/vray_for_blender_rt/src/plugin_exporter/vfb_plugin.writer.cpp
@@ -16,18 +16,21 @@ PluginWriter::PluginWriter(std::string fname, ExportFormat format):
 {
 }
 
-bool PluginWriter::doOpen() {
+bool PluginWriter::doOpen()
+{
 	if (!m_File) {
 		m_File = BLI_fopen(m_FileName.c_str(), "wb");
 	}
 	return m_File != nullptr;
 }
 
-std::string PluginWriter::getName() const {
+std::string PluginWriter::getName() const
+{
 	return fs::path(m_FileName).filename().string();
 }
 
-PluginWriter & PluginWriter::include(std::string name) {
+PluginWriter &PluginWriter::include(std::string name)
+{
 	if (name != m_FileName && !name.empty()) {
 		// dont include self
 		m_Includes.insert(std::move(name));
@@ -35,21 +38,22 @@ PluginWriter & PluginWriter::include(std::string name) {
 	return *this;
 }
 
-PluginWriter::~PluginWriter() {
+PluginWriter::~PluginWriter()
+{
 	if (!m_Includes.empty()) {
 		*this << "\n";
-		for (const auto & inc : m_Includes) {
+		for (const auto &inc : m_Includes) {
 			*this << "#include \"" << inc << "\"\n";
 		}
 	}
-
 
 	if (m_File) {
 		fclose(m_File);
 	}
 }
 
-PluginWriter & PluginWriter::write(const char * format, ...) {
+PluginWriter &PluginWriter::write(const char *format, ...)
+{
 	va_list args;
 
 	va_start(args, format);
@@ -69,67 +73,81 @@ PluginWriter & PluginWriter::write(const char * format, ...) {
 	return *this;
 }
 
-PluginWriter & PluginWriter::writeStr(const char * str) {
+PluginWriter &PluginWriter::writeStr(const char *str)
+{
 	return this->writeData(str, strlen(str));
 }
 
-PluginWriter & PluginWriter::writeData(const void * data, int size) {
+PluginWriter &PluginWriter::writeData(const void *data, int size)
+{
 	if (doOpen()) {
 		fwrite(data, 1, size, m_File);
 	}
 	return *this;
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const int & val) {
+PluginWriter &operator<<(PluginWriter &pp, const int &val)
+{
 	return pp.write("%d", val);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const float & val) {
+PluginWriter &operator<<(PluginWriter &pp, const float &val)
+{
 	return pp.write("%g", val);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const char * val) {
+PluginWriter &operator<<(PluginWriter &pp, const char *val)
+{
 	return pp.writeStr(val);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const std::string & val) {
+PluginWriter &operator<<(PluginWriter &pp, const std::string &val)
+{
 	return pp.writeData(val.c_str(), val.size());
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrColor & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrColor &val)
+{
 	return pp.write("Color(%g, %g, %g)", val.r, val.g, val.b);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrAColor & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrAColor &val)
+{
 	return pp.write("AColor(%g, %g, %g, %g)", val.color.r, val.color.g, val.color.b, val.alpha);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrVector & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrVector &val)
+{
 	return pp.write("Vector(%g, %g, %g)", val.x, val.y, val.z);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrVector2 & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrVector2 &val)
+{
 	return pp.write("Vector2(%g, %g)", val.x, val.y);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrMatrix & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrMatrix &val)
+{
 	return pp << "Matrix(" << val.v0 << "," << val.v1 << "," << val.v2 << ")";
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrTransform & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrTransform &val)
+{
 	return pp << "Transform(" << val.m << "," << val.offs << ")";
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrPlugin & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrPlugin &val)
+{
 	return pp << StripString(val.plugin);
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrMapChannels & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrMapChannels &val)
+{
 	pp << "List(\n";
 
 	if (!val.data.empty()) {
 		auto iter = val.data.cbegin();
-		pp << "List(" << iter->first<< ",\n";
+		pp << "List(" << iter->first << ",\n";
 		pp << iter->second.vertices << "," << iter->second.faces << ")";
 
 		for (; iter != val.data.cend(); ++iter) {
@@ -141,13 +159,14 @@ PluginWriter & operator<<(PluginWriter & pp, const AttrMapChannels & val) {
 	return pp << ")";
 }
 
-PluginWriter & operator<<(PluginWriter & pp, const AttrInstancer & val) {
+PluginWriter &operator<<(PluginWriter &pp, const AttrInstancer &val)
+{
 	pp << "List(" << val.frameNumber;
 
 	if (!val.data.empty()) {
 		pp << ",\n    List(" << (*val.data)[0].index << ", " << (*val.data)[0].tm << ", " << (*val.data)[0].vel << "," << (*val.data)[0].node << ")";
 		for (int c = 1; c < val.data.getCount(); c++) {
-			const auto & item = (*val.data)[c];
+			const auto &item = (*val.data)[c];
 			pp << ",\n    List(" << item.index << ", " << item.tm << ", " << item.vel << "," << item.node << ")";
 		}
 	}
--- intern/vray_for_blender_rt/src/plugin_exporter/vfb_plugin_writer.h
+++ intern/vray_for_blender_rt/src/plugin_exporter/vfb_plugin_writer.h
@@ -23,15 +23,15 @@ public:
 	~PluginWriter();
 
 	PluginWriter(const PluginWriter &) = delete;
-	PluginWriter & operator=(const PluginWriter &) = delete;
+	PluginWriter &operator=(const PluginWriter &) = delete;
 
-	PluginWriter & writeStr(const char * str);
-	PluginWriter & writeData(const void * data, int size);
-	PluginWriter & write(const char * format, ...);
+	PluginWriter &writeStr(const char *str);
+	PluginWriter &writeData(const void *data, int size);
+	PluginWriter &write(const char *format, ...);
 
 	ExportFormat format() const { return m_Format; }
 
-	PluginWriter & include(std::string);
+	PluginWriter &include(std::string);
 	std::string getName() const;
 private:
 
@@ -41,46 +41,50 @@ private:
 	std::set<std::string> m_Includes;
 	ExportFormat m_Format;
 	std::string m_FileName;
-	FILE * m_File;
+	FILE *m_File;
 	std::vector<char> m_Buff;
 };
 
-PluginWriter & operator<<(PluginWriter & pp, const int & val);
-PluginWriter & operator<<(PluginWriter & pp, const float & val);
-PluginWriter & operator<<(PluginWriter & pp, const char * val);
-PluginWriter & operator<<(PluginWriter & pp, const std::string & val);
+PluginWriter &operator<<(PluginWriter &pp, const int &val);
+PluginWriter &operator<<(PluginWriter &pp, const float &val);
+PluginWriter &operator<<(PluginWriter &pp, const char *val);
+PluginWriter &operator<<(PluginWriter &pp, const std::string &val);
 
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrColor & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrAColor & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrVector & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrVector2 & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrMatrix & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrTransform & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrPlugin & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrMapChannels & val);
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrInstancer & val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrColor &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrAColor &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrVector &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrVector2 &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrMatrix &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrTransform &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrPlugin &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrMapChannels &val);
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrInstancer &val);
 
 template <typename T>
 using KVPair = std::pair<std::string, T>;
 
 
 template <typename T>
-PluginWriter & operator<<(PluginWriter & pp, const KVPair<T> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const KVPair<T> &val)
+{
 	return pp << "  " << val.first << "=" << val.second << ";\n";
 }
 
 template <> inline
-PluginWriter & operator<<(PluginWriter & pp, const KVPair<std::string> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const KVPair<std::string> &val)
+{
 	return pp << "  " << val.first << "=\"" << val.second << "\";\n";
 }
 
 template <typename T>
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrSimpleType<T> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrSimpleType<T> &val)
+{
 	return pp << val.m_Value;
 }
 
 template <typename T>
-PluginWriter & printList(PluginWriter & pp, const VRayBaseTypes::AttrList<T> & val, const char * listName, bool newLine = false) {
+PluginWriter &printList(PluginWriter &pp, const VRayBaseTypes::AttrList<T> &val, const char *listName, bool newLine = false)
+{
 	if (!val.empty()) {
 		pp << "List" << listName;
 		if (listName[0] == '\0' || pp.format() == ExportFormat::PLAIN) {
@@ -103,28 +107,29 @@ PluginWriter & printList(PluginWriter & pp, const VRayBaseTypes::AttrList<T> & v
 }
 
 template <typename T>
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrList<T> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrList<T> &val)
+{
 	return printList(pp, val, "", true);
 }
 
 template <> inline
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrList<float> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrList<float> &val)
+{
 	return printList(pp, val, "Float");
 }
 
 template <> inline
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrList<int> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrList<int> &val)
+{
 	return printList(pp, val, "Int");
 }
 
 template <> inline
-PluginWriter & operator<<(PluginWriter & pp, const VRayBaseTypes::AttrList<VRayBaseTypes::AttrVector> & val) {
+PluginWriter &operator<<(PluginWriter &pp, const VRayBaseTypes::AttrList<VRayBaseTypes::AttrVector> &val)
+{
 	return printList(pp, val, "Vector", true);
 }
 
-
 } // VRayForBlender
-
-
 
 #endif // VRAY_FOR_BLENDER_PLUGIN_WRITER_FILE_H
