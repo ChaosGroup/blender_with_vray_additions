1ed54c92d718b1c51c70e8804d3ae719b8a4fe35
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-12-07 14:12:45 +0200

    add: vfb thread manager

---
+++ intern/vray_for_blender_rt/src/vfb_thread_manager.cpp
@@ -0,0 +1,87 @@
+#include "vfb_thread_manager.h"
+#include "cgr_config.h" // PRINT_INFO_EX
+
+#include "BLI_utildefines.h" // BLI_Assert
+
+using namespace VRayForBlender;
+using namespace std;
+
+ThreadManager::ThreadManager(int thCount) {
+	if (thCount > 0) {
+		for (int c = 0; c < thCount; ++c) {
+			m_workers.emplace_back(thread(&ThreadManager::workerRun, this));
+		}
+	}
+}
+
+ThreadManager::Ptr ThreadManager::make(int thCount) {
+	return ThreadManager::Ptr(new ThreadManager(thCount));
+}
+
+ThreadManager::~ThreadManager() {
+	// m_workers is not protected because there is no sane way to do this from inside the ThreadManager
+	// it can't handle calling some methond and dtor concurrently
+	if (m_workers.size() > 0) {
+		BLI_assert(!"VFB ThreadManager exiting while threads are running!");
+		stop();
+	}
+}
+
+void ThreadManager::stop() {
+	m_stop = true;
+
+	if (!m_workers.empty()) {
+		m_queueCondVar.notify_all();
+
+		for (int c = 0; c < m_workers.size(); ++c) {
+			if (m_workers[c].joinable()) {
+				m_workers[c].join();
+			} else {
+				BLI_assert(!"VFB ThreadManager's thread is not joinable during stop!");
+			}
+		}
+	}
+}
+
+void ThreadManager::addTask(ThreadManager::Task task, ThreadManager::Priority priority) {
+	if (m_workers.empty()) {
+		// no workers - do the job ourselves
+		task(this_thread::get_id(), m_stop);
+	} else {
+		{
+			lock_guard<mutex> lock(m_queueMtx);
+			if (priority == Priority::HIGH) {
+				m_tasks.push_front(task);
+			} else {
+				m_tasks.push_back(task);
+			}
+		}
+		m_queueCondVar.notify_one();
+	}
+}
+
+void ThreadManager::workerRun() {
+	auto id = this_thread::get_id();
+
+	PRINT_INFO_EX("Thread [%u] starting ...", id.hash());
+
+	while (!m_stop) {
+		Task task;
+		{
+			unique_lock<mutex> lock(m_queueMtx);
+			// wait for task or stop
+			m_queueCondVar.wait(lock, [this] { return !m_tasks.empty() || m_stop; });
+
+			if (m_stop) {
+				break;
+			}
+
+			task = m_tasks.front();
+			m_tasks.pop_front();
+		}
+
+		task(id, m_stop);
+	}
+
+	PRINT_INFO_EX("Thread [%u] stopping.", id.hash());
+}
---
+++ intern/vray_for_blender_rt/src/vfb_thread_manager.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2015, Chaos Software Ltd
+ *
+ * V-Ray For Blender
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef VRAY_FOR_BLENDER_THREAD_MANAGER_H
+#define VRAY_FOR_BLENDER_THREAD_MANAGER_H
+
+#include <functional>
+#include "BLI_task.h"
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include <memory>
+#include <deque>
+#include <vector>
+
+namespace VRayForBlender {
+
+class ThreadManager {
+public:
+	typedef std::shared_ptr<ThreadManager> Ptr;
+
+	// intended to be used with lambda which will capture all neded data
+	// can check current thread id and must obey stop flag asap
+	typedef std::function<void(std::thread::id id, const volatile bool & stop)> Task;
+
+	enum class Priority {
+		LOW, HIGH,
+	};
+
+	// create new Thread Manager with @thCount threads running
+	// thCount 0 will mean that addTask will block and complete the task on the current thread
+	static Ptr make(int thCount);
+
+	ThreadManager(const ThreadManager &) = delete;
+	ThreadManager & operator=(const ThreadManager &) = delete;
+
+	// stop must be called before reaching dtor
+	~ThreadManager();
+
+	// stops all threads and discards any tasks not yet started
+	// if thread count is 0, stop will still set the flag for stop to true
+	// and if addTask was called from another thread it will signal the task to stop
+	void stop();
+
+	// @task with LOW  @priority will be added at the end      of queue
+	// @task with HIGH @priority will be added at the begining of queue
+	void addTask(Task task, Priority priority);
+private:
+	ThreadManager(int thCount);
+
+	void workerRun();
+
+	std::mutex               m_queueMtx;
+	std::condition_variable  m_queueCondVar;
+	std::deque<Task>         m_tasks;
+	std::vector<std::thread> m_workers;
+	volatile bool            m_stop;
+};
+
+} // namespace VRayForBlender
+
+#endif // VRAY_FOR_BLENDER_THREAD_MANAGER_H
