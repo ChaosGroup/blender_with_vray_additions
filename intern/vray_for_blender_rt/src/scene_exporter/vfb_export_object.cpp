a40f8873578e7a4ed868da0c6d928ac8ec77b2bb
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-12-21 15:12:13 +0200

    add: support for MtlRenderStats per ob

--- intern/vray_for_blender_rt/src/scene_exporter/vfb_export_object.cpp
+++ intern/vray_for_blender_rt/src/scene_exporter/vfb_export_object.cpp
@@ -303,6 +303,21 @@ AttrValue DataExporter::exportObject(BL::Object ob, bool check_updated, const Ob
 			mtl = getDefaultMaterial();
 		}
 
+		PointerRNA vrayObject = RNA_pointer_get(&ob.ptr, "vray");
+		PointerRNA mtlRenderStats = RNA_pointer_get(&vrayObject, "MtlRenderStats");
+
+		if (RNA_boolean_get(&mtlRenderStats, "use")) {
+			PluginDesc genericWrapper("MtlRenderStats@" + exportName, "MtlRenderStats");
+			genericWrapper.add("base_mtl", mtl);
+
+			const char * attrsNames[] = {"visibility", "camera_visibility", "gi_visibility", "shadows_visibility", "reflections_visibility", "refractions_visibility"};
+			for (const auto name : attrsNames) {
+				genericWrapper.add(name, RNA_boolean_get(&mtlRenderStats, name));
+			}
+
+			mtl = m_exporter->export_plugin(genericWrapper);
+		}
+
 		if (geom && mtl && (is_updated || is_data_updated || m_layer_changed)) {
 			// No need to export Node if the object is LightMesh
 			if (!isMeshLight) {
