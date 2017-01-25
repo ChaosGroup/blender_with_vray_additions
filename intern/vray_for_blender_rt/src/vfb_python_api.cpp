aab0a762d39c03bcf5ddb9a22b81889ed39fb3f6
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2017-01-25 18:24:56 +0200

    fix: deadlock on zmq stop

--- intern/vray_for_blender_rt/src/vfb_python_api.cpp
+++ intern/vray_for_blender_rt/src/vfb_python_api.cpp
@@ -86,9 +86,10 @@ static PyObject* vfb_zmq_heartbeat_stop(PyObject*)
 {
 	std::lock_guard<std::mutex> l(heartbeatLock);
 	if (heartbeatClient) {
-		PRINT_INFO_EX("Stopping hearbeat client");
+		PRINT_INFO_EX("Stopping hearbeat client... ");
 		heartbeatClient->syncStop();
 		heartbeatClient.reset();
+		PRINT_INFO_EX("... done.");
 	} else {
 		PRINT_ERROR("No zmq heartbeat client running...");
 	}
