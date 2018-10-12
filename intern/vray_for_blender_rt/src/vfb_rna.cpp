200f75b11c8fa168cf5d7b89c3c18676f2a81bb5
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-10-12 16:29:26 +0300

    fix: use buf[] for RNA_std_string_get

--- intern/vray_for_blender_rt/src/vfb_rna.cpp
+++ intern/vray_for_blender_rt/src/vfb_rna.cpp
@@ -21,89 +21,77 @@
 
 #include "DNA_ID.h"
 #include "BLI_path_util.h"
+#include "BLI_fileops.h"
 #include "BKE_global.h"
 #include "BKE_main.h"
 
 #include "rna_internal_types.h"
 
-
 using namespace VRayForBlender;
-
 
 std::string VRayForBlender::RNA_path_get(PointerRNA *ptr, const std::string &attrName)
 {
-	char filepath[FILE_MAX] = "";
-
-	RNA_string_get(ptr, attrName.c_str(), filepath);
-	BLI_path_abs(filepath, ID_BLEND_PATH(G.main, ((ID*)ptr->id.data)));
-
-	return filepath;
+	char buf[FILE_MAX] = "";
+	RNA_string_get(ptr, attrName.c_str(), buf);
+	BLI_path_abs(buf, ID_BLEND_PATH(G.main, ((ID*)ptr->id.data)));
+	return std::string(buf);
 }
-
 
 std::string VRayForBlender::RNA_std_string_get(PointerRNA *ptr, const std::string &attrName)
 {
-	std::string result;
-
-	const int len = RNA_string_length(ptr, attrName.c_str());
-	char * dest = new char[len + 1];
-	RNA_string_get(ptr, attrName.c_str(), dest);
-	result = dest;
-	delete[] dest;
-
-	return result;
+	char buf[PATH_MAX] = "";
+	RNA_string_get(ptr, attrName.c_str(), buf);
+	return std::string(buf);
 }
-
 
 const EnumPropertyItem *VRayForBlender::RNA_enum_item(PointerRNA *ptr, const char *attrName)
 {
-	int enum_item_index = RNA_enum_get(ptr, attrName);
-
-	const EnumPropertyRNA *enum_prop = (const EnumPropertyRNA*)RNA_struct_find_property(ptr, attrName);
-	if (enum_prop)
-		return &enum_prop->item[enum_item_index];
-
+	const EnumPropertyRNA *enumProp =
+		reinterpret_cast<const EnumPropertyRNA*>(RNA_struct_find_property(ptr, attrName));
+	if (enumProp) {
+		const int enumItemIndex = RNA_enum_get(ptr, attrName);
+		return &enumProp->item[enumItemIndex];
+	}
 	return NULL;
 }
 
-
 std::string VRayForBlender::RNA_enum_identifier_get(PointerRNA *ptr, const char *attrName)
 {
-	const EnumPropertyItem *enum_item = RNA_enum_item(ptr, attrName);
-	if (enum_item->identifier)
-		return enum_item->identifier;
-	return "";
+	const EnumPropertyItem *enumItem = RNA_enum_item(ptr, attrName);
+	if (enumItem->identifier && *enumItem->identifier) {
+		return enumItem->identifier;
+	}
+	return std::string("");
 }
-
 
 std::string VRayForBlender::RNA_enum_name_get(PointerRNA *ptr, const char *attrName)
 {
-	const EnumPropertyItem *enum_item = RNA_enum_item(ptr, attrName);
-	if (enum_item->name)
-		return enum_item->name;
-	return "";
+	const EnumPropertyItem *enumItem = RNA_enum_item(ptr, attrName);
+	if (enumItem->name && *enumItem->name) {
+		return enumItem->name;
+	}
+	return std::string("");
 }
-
 
 int VRayForBlender::RNA_enum_ext_get(PointerRNA *ptr, const char *attrName)
 {
-	int enum_item_index = RNA_enum_get(ptr, attrName);
+	int enumItemIndex = RNA_enum_get(ptr, attrName);
 
-	const EnumPropertyItem *enum_item = RNA_enum_item(ptr, attrName);
-	if (!enum_item->identifier) {
+	const EnumPropertyItem *enumItem = RNA_enum_item(ptr, attrName);
+	if (!enumItem->identifier) {
 		PropertyRNA *prop = RNA_struct_find_property(ptr, attrName);
 		if (prop) {
 			PRINT_ERROR("Property \"%s\": Enum identifier is not found!",
-						prop->name);
+				prop->name);
 		}
 	}
 	else {
 		// If enum item is digit, return it as int
 		int tmp_int = 0;
-		if (sscanf(enum_item->identifier, "%i", &tmp_int) == 1) {
-			enum_item_index = tmp_int;
+		if (sscanf(enumItem->identifier, "%i", &tmp_int) == 1) {
+			enumItemIndex = tmp_int;
 		}
 	}
 
-	return enum_item_index;
+	return enumItemIndex;
 }
